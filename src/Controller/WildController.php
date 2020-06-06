209e40b8b81271f13bbfe8ee25db02fa6376b52d
raym648 <raymond.francius@gmail.com>
2020-06-06 08:55:32 +0200

    quete 08 - Symfony : Récupérer des données stockées avec Doctrine

---
+++ src/Controller/WildController.php
@@ -0,0 +1,74 @@
+<?php
+
+// src/Controller/WildController.php
+
+namespace App\Controller;
+
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class WildController extends AbstractController
+{
+    /**
+      * Show all rows from Program’s entity
+      *
+      * @Route("/", name="wild_index")
+      * @return Response A response instance
+      */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+            'No program found in program\'s table.'
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
+    }
+
+
+    /**
+      * Getting a program with a formatted slug for title
+      *
+      * @param string $slug The slugger
+      * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="show")
+      * @return Response
+      */
+    public function show(?string $slug):Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'program' => $program,
+            'slug'  => $slug,
+        ]);
+    }
+   
+}
